f1e953ded66b72d4cd477b0dd1df59bc6a517fbf
alex <69579692+alxwnth@users.noreply.github.com>
2022-12-03 23:42:24 +0200

    Update HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
-import PlayerBox from '@/components/PlayerBox.vue'
 import NowPlaying from '@/components/NowPlaying.vue'
 </script>
 
 <template>
-  <PlayerBox></PlayerBox>
   <NowPlaying></NowPlaying>
 </template>
+<style scoped>
+.title {
+  padding-top: 5.5em;
+  text-align: center;
+  color: #fff8e4;
+  font-size: 2.1em;
+  font-family: auto-pro-small-caps, sans-serif;
+  font-weight: 900;
+  font-style: normal;
+  text-shadow: 1px 1px 4px #4f4732;
+}
+
+.beta {
+  font-size: 0.5em;
+  font-weight: 400;
+  vertical-align: sub;
+}
+</style>
